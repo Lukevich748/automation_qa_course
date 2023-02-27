baa0d8590d686293c2f47a948b4cc2696bfefaab
Lukevich <luckevich_94@mail.ru>
2023-02-27 16:38:30 +0100

    add a new func "test_text_box"

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,17 @@
 import time
-from pages.base_page import BasePage
+
+from pages.elements_page import TextBoxPage
 
 
-def test(driver):
-    page = BasePage(driver, "https://demoqa.com/text-box")
-    page.open()
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
+            assert full_name == output_name, "The full name does not match"
+            assert email == output_email, "The email does not match"
+            assert current_address == output_current_address, "The current address does not match"
+            assert permanent_address == output_permanent_address, "The permanent address does not match"
