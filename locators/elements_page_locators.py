a9aa318bbae5f97c60bfcfa555bbf7d4bb790202
Lukevich <luckevich_94@mail.ru>
2023-02-28 13:23:25 +0100

    added new class "CheckBoxPageLocators"

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -15,3 +15,11 @@ class TextBoxPageLocators:
     CREATED_EMAIL = (By.XPATH, "//p[@id='email']")
     CREATED_CURRENT_ADDRESS = (By.XPATH, "//p[@id='currentAddress']")
     CREATED_PERMANENT_ADDRESS = (By.XPATH, "//p[@id='permanentAddress']")
+
+class CheckBoxPageLocators:
+
+    EXPAND_ALL_BUTTON = (By.XPATH, "//button[@aria-label='Expand all']")
+    ITEM_LIST = (By.XPATH, "//span[@class='rct-title']")
+    CHECKED_ITEM = (By.XPATH, "//*[@class='rct-icon rct-icon-check']")
+    TITLE_ITEM = (By.XPATH, "//*[@class='rct-icon rct-icon-check']//ancestor::span//span[@class='rct-title']")
+    OUTPUT_RESULT = (By.XPATH, "//span[@class='text-success']")
