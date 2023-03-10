87c2005f0acbee002b0a166a6b0b4b0301c81dfe
Lukevich <luckevich_94@mail.ru>
2023-03-10 15:45:06 +0100

    added description of errors

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -11,24 +11,24 @@ def test_alerts(driver):
     first_name = person_info.firstname
     alerts_page.click_alert_button()
     alert_text = alerts_page.get_alert_text()
-    assert alert_text == "You clicked a button"
+    assert alert_text == "You clicked a button", "invalid text"
     alerts_page.accept_alert()
     alerts_page.click_time_alert_button()
     appear_alert_text = alerts_page.get_alert_text()
-    assert appear_alert_text == "This alert appeared after 5 seconds"
+    assert appear_alert_text == "This alert appeared after 5 seconds", "invalid text"
     alerts_page.accept_alert()
     alerts_page.click_confirm_alert_button()
     confirm_alert_text = alerts_page.get_alert_text()
-    assert confirm_alert_text == "Do you confirm action?"
+    assert confirm_alert_text == "Do you confirm action?", "invalid text"
     alerts_page.accept_alert()
     ok_confirm_result = alerts_page.get_confirm_alert_button_result()
-    assert ok_confirm_result == "You selected Ok"
+    assert ok_confirm_result == "You selected Ok", "invalid text"
     alerts_page.click_confirm_alert_button()
     alerts_page.dismiss_alert()
     cancel_confirm_result = alerts_page.get_confirm_alert_button_result()
-    assert cancel_confirm_result == "You selected Cancel"
+    assert cancel_confirm_result == "You selected Cancel", "invalid text"
     alerts_page.click_prompt_alert_button()
     alerts_page.input_text_in_alert(first_name)
     alerts_page.accept_alert()
     prompt_alert_button_result = alerts_page.get_prompt_alert_button_result()
-    assert "You entered " + first_name == prompt_alert_button_result
+    assert "You entered " + first_name == prompt_alert_button_result, "invalid text"
